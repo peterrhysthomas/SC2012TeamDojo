e8afb3696d82b8d0e0b2268f6ec74553138233d1
unknown <LD@Dell_Inspiron.(none)>
2012-06-14 15:42:26 +0100

    initial xml slurping test - acceptance test 1

--- src/main/java/sc2012/teamdojo/Programmer.java
+++ src/main/java/sc2012/teamdojo/Programmer.java
@@ -1,10 +1,15 @@
 package sc2012.teamdojo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Programmer {
 	private String name;
+    private List<String> skills = new ArrayList<String>();
+    private List<String> recommends = new ArrayList<String>();
 	//private List recommendations;
+
+
 
 	public String getName() {
 		return name;
@@ -12,6 +17,15 @@ public class Programmer {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+    public void addRecommend(String recommend) {
+       recommends.add(recommend);
+    }
+
+    public void addSkill(String skill) {
+        recommends.add(skill);
+    }
+
 
 
 }
--- src/test/groovy/sc2012/teamdojo/ProNetTests.groovy
+++ src/test/groovy/sc2012/teamdojo/ProNetTests.groovy
@@ -1,4 +1,4 @@
-:qpackage sc2012.teamdojo
+package sc2012.teamdojo
 
 import org.codehaus.groovy.tools.ast.TestHarnessClassLoader;
 import org.junit.Test;
@@ -7,8 +7,24 @@ class ProNetTests {
 	
 	@Test
     void dummy(){
-		def records = new XmlSlurper().parse( new File(this.getClass().getClassLoader().getResource("ProNet.xml").getPath()));
-		println records
-		
+		def network = new XmlSlurper().parse( new File(this.getClass().getResource("/ProNet.xml").getPath()));
+        def programmers = []
+
+        network.Programmer.each { prog ->
+            Programmer programmer = new Programmer()
+            programmer.name = prog.@name
+            prog.Recommendations.each { recommend ->
+                programmer.recommends.add(recommend)
+            }
+            prog.Skills.each { skill ->
+                programmer.skills.add(skill)
+            }
+            programmers.add(programmer)
+        }
+
+        programmers.each {
+            println it.name + ' ' + it.skills + ' ' + it.recommends
+        }
+
 	}
 }
