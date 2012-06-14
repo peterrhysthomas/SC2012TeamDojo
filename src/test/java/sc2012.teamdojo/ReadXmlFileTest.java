17d467b732fc5e3bfcfa4e0a34619fa6b64e1aed
Peter Suggitt <me@suggs.org.uk>
2012-06-14 15:53:17 +0100

    Updated pom

---
+++ src/test/java/sc2012.teamdojo/ReadXmlFileTest.java
@@ -0,0 +1,11 @@
+package sc2012.teamdojo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: suggitpe
+ * Date: 14/06/12
+ * Time: 15:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class ReadXmlFileTest {
+}
