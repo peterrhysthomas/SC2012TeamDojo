abc434a811a099d2733db63e75a6e06bf459f893
Dan <dan@danMBP.local>
2012-06-14 15:09:43 +0100

    add programmer

---
+++ src/main/java/sc2012/teamdojo/Programmer.java
@@ -0,0 +1,17 @@
+package sc2012.teamdojo;
+
+import java.util.List;
+
+public class Programmer {
+	private String name;
+	//private List recommendations;
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+}
