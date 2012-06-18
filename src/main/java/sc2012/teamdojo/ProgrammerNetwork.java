5b6f51d19136695e038ba3fa258cbf6dd7c2f06d
Michael <michael.hempenstall@gmail.com>
2012-06-18 23:56:25 +0100

    Add todo's to the network/graph implementation...

--- src/main/java/sc2012/teamdojo/ProgrammerNetwork.java
+++ src/main/java/sc2012/teamdojo/ProgrammerNetwork.java
@@ -1,5 +1,13 @@
 package sc2012.teamdojo;
 
+/**
+ * 
+ * TODO: 
+ * 1. finish java implementation of network/graph
+ * 2. re-write as groovy
+ * 3. throw all that away and use http://jgrapht.org (thank you filippo...)
+ *
+ */
 public class ProgrammerNetwork {
 	private Programmer[] programmers;
 	private Edge[][] programmerConnections;
